386cb63b454d4800b97282bac5a388321a32e361
Steve Warren <steve@interactivenyc.com>
2018-10-31 16:38:57 -0400

    api route protection for users

--- server/api/users.js
+++ server/api/users.js
@@ -3,14 +3,23 @@ const {User} = require('../db/models')
 module.exports = router
 
 router.get('/', async (req, res, next) => {
+  const isAdmin = req.user.dataValues.isAdmin || false
+
+  console.log('[API] GET USERS req.user isAdmin', isAdmin, req.user.dataValues)
+
   try {
-    const users = await User.findAll({
-      // explicitly select only the id and email fields - even though
-      // users' passwords are encrypted, it won't help if we just
-      // send everything to anyone who asks!
-      // attributes: ['id', 'email']
-    })
-    res.json(users)
+    if (isAdmin) {
+      let users = await User.findAll({
+        // explicitly select only the id and email fields - even though
+        // users' passwords are encrypted, it won't help if we just
+        // send everything to anyone who asks!
+        // attributes: ['id', 'email']
+      })
+      console.log('[API] GET USERS isAdmin - showing results')
+      res.json(users)
+    } else {
+      res.send("sorry, you don't have access to this feature")
+    }
   } catch (err) {
     next(err)
   }
