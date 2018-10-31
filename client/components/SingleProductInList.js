61ee5fbc2a07a309ff4b90af2e84cb666e694bea
Kirsten Chong <kirstch7@gmail.com>
2018-10-31 15:14:36 -0400

    Fixes routes

--- client/components/SingleProductInList.js
+++ client/components/SingleProductInList.js
@@ -2,18 +2,19 @@ import React from 'react'
 import {Link} from 'react-router-dom'
 
 function SingleProductInList(props) {
+  const {photo, id, title, price} = props
   return (
     <tr>
       <td>
-        <Link to={`/products/${props.id}`}>
-          <img src={props.photo} />
+        <Link to={`/products/${id}`}>
+          <img src={photo} />
         </Link>
       </td>
       <td>
-        <Link to={`/products/${props.id}`}>
-          <div>{props.title}</div>
+        <Link to={`/products/${id}`}>
+          <div>{title}</div>
         </Link>
-        <div>Price: ${props.price}</div>
+        <div>Price: ${price}</div>
       </td>
     </tr>
   )
