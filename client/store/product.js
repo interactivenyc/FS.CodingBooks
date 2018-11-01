978bb02d70e9afc5eb2cf34594895854c6ab951c
Steve Warren <steve@interactivenyc.com>
2018-11-01 18:02:58 -0400

    fixed strange merge conflict

--- client/store/product.js
+++ client/store/product.js
@@ -9,19 +9,14 @@ const FILTER_PRODUCT = 'FILTER_PRODUCT'
 const REMOVE_PRODUCT = 'REMOVE_PRODUCT'
 
 /**
-<<<<<<< HEAD
-=======
  * INITIAL STATE
  */
 const defaultProduct = []
 
 /**
->>>>>>> master
  * ACTION CREATORS
  */
 const getProduct = product => ({type: GET_PRODUCT, product})
-const filterProducts = products => ({type: FILTER_PRODUCT, products})
-// const removeProduct = id => ({type: REMOVE_PRODUCT, id})
 
 /**
  * THUNK CREATORS
