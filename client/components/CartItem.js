e5c34156d7ef66673af5c9814c6d7a042f2fca16
Patrick Monaghan <monaghanp3@gmail.com>
2018-11-01 16:16:00 -0400

    added quantities to cart component

--- client/components/CartItem.js
+++ client/components/CartItem.js
@@ -1,23 +1,25 @@
 import React from 'react'
 
-export default function CartItem (props) {
-    const { id, photo, title, price } = props
-    return (
-        <tr>
-            <td className="ui small image"><img src={photo}></img></td>
-                <td>
-                    <div>
-                        <div className="header">{title}</div>
-                        <div className="meta">
-                            <span className="price">by This Author</span>
-                            <span className="stay"></span>
-                        </div>
-                        <div className="description">
-                            <p>Quantity: 1</p>
-                        </div>
-                    </div>
-                </td>
-            <td>$ {price}</td>
-        </tr>
-    )
+export default function CartItem(props) {
+  const {photo, title, price, quantity} = props
+  return (
+    <tr>
+      <td className="ui small image">
+        <img src={photo} />
+      </td>
+      <td>
+        <div>
+          <div className="header">{title}</div>
+          <div className="meta">
+            <span className="price">by This Author</span>
+            <span className="stay" />
+          </div>
+          <div className="description">
+            <p>Quantity: {quantity}</p>
+          </div>
+        </div>
+      </td>
+      <td>$ {price}</td>
+    </tr>
+  )
 }
