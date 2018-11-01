a3491223edf5686f4a09b2a00bd988fbd53e99cb
Patrick Monaghan <monaghanp3@gmail.com>
2018-11-01 13:58:01 -0400

    fixed cart product association table

--- server/api/carts.js
+++ server/api/carts.js
@@ -1,5 +1,5 @@
 const router = require('express').Router()
-const {CartProducts, Product, Cart} = require('../db/models')
+const {CartProducts} = require('../db/models')
 
 module.exports = router
 
@@ -8,6 +8,21 @@ router.get('/:id', async (req, res, next) => {
   try {
     const cart = await CartProducts.findAll({where: {cartId: req.params.id}})
     res.json(cart)
+  } catch (err) {
+    next(err)
+  }
+})
+
+// api/carts/remove/:productId
+router.delete('/remove/:productId', async (req, res, next) => {
+  try {
+    const itemToDelete = await CartProducts.findOne({
+      where: {cartId: req.user.cartId, productId: req.params.productId}
+    })
+    const deletedItem = await CartProducts.destroy({
+      where: {id: itemToDelete.id}
+    })
+    console.log(deletedItem)
   } catch (err) {
     next(err)
   }
