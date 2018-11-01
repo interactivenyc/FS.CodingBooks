cc6bc70394ddbc60415b7060c26dd0ab68395352
Loren Mariquit <l.mariquit@gmail.com>
2018-11-01 18:21:58 -0400

    more modular signup

--- client/store/user.js
+++ client/store/user.js
@@ -43,21 +43,15 @@ export const me = () => async dispatch => {
 }
 
 export const auth = (
-  email,
-  password,
-  method,
-  firstName,
-  lastName
+obj
 ) => async dispatch => {
   let res
+  const {email, password, method} = obj
+  console.log('GOT TO AUTH DISPATCH. USER.JS. method: ', obj)
   try {
     if (method === 'signup') {
-      res = await axios.post(`/auth/${method}`, {
-        email,
-        password,
-        firstName,
-        lastName
-      })
+      console.log('IN SIGN IN AXIOS')
+      res = await axios.post(`/auth/${method}`, obj)
     } else {
       res = await axios.post(`/auth/${method}`, {email, password})
     }
