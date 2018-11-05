fbbc2ad62dce261fdda00f56cd772429efd3e45d
Steve Warren <steve@interactivenyc.com>
2018-11-05 14:23:28 -0500

    adding more books with categories, filter view

--- client/components/ProductList.js
+++ client/components/ProductList.js
@@ -1,11 +1,25 @@
 import React from 'react'
 import {connect} from 'react-redux'
-import {Link} from 'react-router-dom'
 import {NavDropdown, MenuItem} from 'react-bootstrap'
 import {selectCategory} from '../store/category'
 import SingleProductInList from './SingleProductInList'
 
 class ProductList extends React.Component {
+  constructor() {
+    super()
+    this.categorySelect = this.categorySelect.bind(this)
+    this.state = {
+      categoryId: 0
+    }
+  }
+
+  categorySelect(categoryId) {
+    console.log('[ProductList] categorySelect', categoryId)
+
+    this.setState({
+      categoryId
+    })
+  }
   render() {
     const allProductsArr = this.props.product
     const allCategoriesArr = this.props.allCategory
@@ -14,10 +28,11 @@ class ProductList extends React.Component {
     return (
       <div>
         <NavDropdown title="Select a category to filter" id="nav-dropdown">
+          <MenuItem onSelect={() => this.categorySelect(0)}>All</MenuItem>
           {allCategoriesArr.map(category => (
             <MenuItem
               key={category.id}
-              onSelect={() => this.props.selectCategory(category.id)}
+              onSelect={() => this.categorySelect(category.id)}
             >
               {category.name}
             </MenuItem>
@@ -27,18 +42,26 @@ class ProductList extends React.Component {
           <div className="ui message left aligned grid">
             <div className="ui list">
               {allProductsArr[0] &&
-                allProductsArr.map(product => {
-                  return (
-                    <SingleProductInList
-                      title={product.title}
-                      context="product-list"
-                      productId={product.id}
-                      photo={product.photo}
-                      price={product.price}
-                      key={keyIndex++}
-                    />
-                  )
-                })}
+                allProductsArr
+                  .filter(product => {
+                    return (
+                      product.categoryId === this.state.categoryId ||
+                      this.state.categoryId === 0
+                    )
+                  })
+                  .map(product => {
+                    return (
+                      <SingleProductInList
+                        title={product.title}
+                        context="product-list"
+                        productId={product.id}
+                        photo={product.photo}
+                        price={product.price}
+                        categoryId={product.categoryId}
+                        key={keyIndex++}
+                      />
+                    )
+                  })}
             </div>
           </div>
         </div>
