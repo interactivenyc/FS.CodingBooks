37602504a35cc818767e2b3141ec3e6b0e74aa22
Loren Mariquit <l.mariquit@gmail.com>
2018-11-06 17:44:02 -0500

    edited view in order list

--- client/components/OrderList.js
+++ client/components/OrderList.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {connect} from 'react-redux'
+import { connect } from 'react-redux'
 import SingleOrder from './SingleOrder'
 import axios from 'axios'
 
@@ -13,22 +13,22 @@ class OrderList extends React.Component {
 
   async componentDidMount() {
     if (this.props.cartId) {
-      const {data} = await axios.get(`/api/users/orders/${this.props.cartId}`)
-      this.setState({orderList: data})
+      const { data } = await axios.get(`/api/users/orders/${this.props.cartId}`)
+      this.setState({ orderList: data })
       console.log(this.state.orderList)
     }
   }
 
   async componentDidUpdate(prevProps) {
     if (this.props.cartId !== prevProps.cartId) {
-      const {data} = await axios.get(`/api/users/orders/${this.props.cartId}`)
-      this.setState({orderList: data})
+      const { data } = await axios.get(`/api/users/orders/${this.props.cartId}`)
+      this.setState({ orderList: data })
       console.log(this.state.orderList)
     }
   }
 
   render() {
-    const uniqueOrder = (function(orderList) {
+    const uniqueOrder = (function (orderList) {
       let uniqueOrder = []
       for (let order of orderList) {
         if (!uniqueOrder.includes(order.payDate)) {
@@ -41,25 +41,23 @@ class OrderList extends React.Component {
     return (
       <div>
         {this.state.orderList.length > 0 ? (
-          <div className="ui container" id="narrow" style={{margin: '20px'}}>
-            <div className="ui message left aligned grid">
-              <div className="ui list" />
-              {uniqueOrder.map(orderDate => (
-                <div key={orderDate}>
-                  <div>Ordered on: {orderDate}</div>
-                  <SingleOrder
-                    products={this.props.products}
-                    order={this.state.orderList.filter(
-                      order => order.payDate === orderDate
-                    )}
-                  />
-                </div>
-              ))}
-            </div>
+          <div className="ui container" id="narrow" style={{ margin: '20px' }}>
+            <div className="ui list" />
+            {uniqueOrder.map(orderDate => (
+              <div key={orderDate}>
+                <div>Ordered on: {orderDate}</div>
+                <SingleOrder
+                  products={this.props.products}
+                  order={this.state.orderList.filter(
+                    order => order.payDate === orderDate
+                  )}
+                />
+              </div>
+            ))}
           </div>
         ) : (
-          <div className="listingHeader">You have not placed any orders</div>
-        )}
+            <div className="listingHeader">You have not placed any orders</div>
+          )}
       </div>
     )
   }
